1c627ca59d5e49d40ba51d2de7fd35d816031e3d
Kunduz Rahat <rahatova.kunduz@gmail.com>
2022-08-08 21:09:05 +0600

    first commit

---
+++ src/views/AuthPage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout';
+interface AuthPageProps {
+	
+}
+ 
+const AuthPage: React.FC<AuthPageProps> = (props) => {
+	return (  
+		<Layout>
+			<p>AuthPage</p>
+		</Layout>
+	);
+}
+ 
+export default AuthPage;
---
+++ src/views/ProfilePage/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '../../components/Layout';
+interface ProfilePageProps {
+	
+}
+ 
+const ProfilePage: React.FC<ProfilePageProps> = (props) => {
+	return (  
+		<Layout>
+			ProfilePage
+		</Layout>
+	);
+}
+ 
+export default ProfilePage;
